8e694412324937c80a43b2447de16eb97406ee06
Aldrin Cael <aldrinriancael@gmail.com>
2025-08-31 06:00:18 +0800

    Update Codebase

---
+++ registration/upload.php
@@ -0,0 +1,14 @@
+<?php
+include __DIR__ ."../../../auth/authenticator.php";
+if ($_SERVER["REUEST_METHOD"] == "POST" && isset($_POST["submitbt"])) {
+    $username = $_POST["username"];
+    $password = password_hash($_POST["password"]);
+    $email = $_POST["email"];
+
+    $stm = $conn->prepare("INSERT INTO users (usernmae, email, password) VALUES(?, ?, ?)");
+    $stm ->bind_param("sss", $username,$email, $password);
+    if ($stm -> execute()) {
+        header("Location: index.html?succes=1");
+    }
+}
+?>
